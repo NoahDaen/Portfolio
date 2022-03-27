be86ae37731764cb4afe1c468f93bb16f7e21818
Noah Daen <daennoah@gmail.com>
2022-03-27 22:48:50 +0200

    Debut du projet avec la navbar en css

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <title> Accueil </title>
+</head>
+
+<body class="">
+    <nav class="navbar">
+        <a href="#" class="logo">Logo</a>
+        <label for="btn" class="icon">
+            <svg viewBox="0 0 100 80" width="40" height="40">
+                <rect width="100" height="15"> </rect>
+                <rect y="35" width="100" height="15"> </rect>
+                <rect y="70" width="100" height="15"> </rect>
+            </svg>
+        </label>
+        <input type="checkbox" id="btn">
+        <ul class="nav-menu">
+            <li class="nav-item">
+                <a href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a href="#">Présenation</a>
+            </li>
+            <li class="nav-item">
+                <a href="#">Mes Projets</a>
+            </li>
+            <li class="nav-item">
+                <a href="#">Me contacter</a>
+            </li>
+        </ul>
+    </nav>
+</body>
